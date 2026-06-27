6705f5c857ddaeec1373b8e098cf1b00eda31aa0
prudenceAnalyst <prudencesinzo@gmail.com>
2026-06-27 04:57:35 +0200

    Create 5_Magnitude_analysis.sql

---
+++ Scripts/5_Magnitude_analysis.sql
@@ -0,0 +1,125 @@
+/*
+===============================================================================
+Magnitude Analysis
+===============================================================================
+Purpose:
+    - To quantify data and group results by specific dimensions.
+    - For understanding data distribution across categories.
+
+SQL Functions Used:
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+    - GROUP BY, ORDER BY
+===============================================================================
+*/
+
+-- Find total patients by gender
+SELECT
+    Gender,
+    COUNT(Gender) AS total_patients
+FROM gold.dim_patients
+GROUP BY Gender
+ORDER BY total_patients DESC;
+
+-- Find total patients by age_range
+SELECT
+    age_range,
+    COUNT(age_range) AS total_patients
+FROM gold.dim_patients
+GROUP BY age_range
+ORDER BY total_patients DESC;
+
+-- Find total patients by address
+SELECT
+    Patient_adrress,
+    COUNT(Patient_adrress) AS total_patients
+FROM gold.dim_patients
+GROUP BY Patient_adrress
+ORDER BY total_patients DESC;
+
+-- Find total patients by Stay_segment
+SELECT
+    Stay_segment,
+    COUNT(Stay_segment) AS total_patients
+FROM gold.dim_patients
+GROUP BY Stay_segment
+ORDER BY total_patients DESC;
+
+-- Find total patients by insurance_provider
+SELECT
+    insurance_provider,
+    COUNT(insurance_provider) AS total_patients
+FROM gold.dim_patients
+GROUP BY insurance_provider
+ORDER BY total_patients DESC;
+
+-- Find total patients by Diagnosis
+SELECT
+    Diagnosis,
+    COUNT(Diagnosis) AS total_patients
+FROM gold.dim_patients
+GROUP BY Diagnosis
+ORDER BY total_patients DESC;
+
+-- Find total patients by patients outcome
+SELECT
+    patient_outcome,
+    COUNT(patient_outcome) AS total_patients
+FROM gold.dim_patients
+GROUP BY patient_outcome
+ORDER BY total_patients DESC;
+
+-- Find total patients by Satisfaction
+SELECT
+    Satisfaction,
+    COUNT(Satisfaction) AS total_patients
+FROM gold.dim_patients
+GROUP BY Satisfaction
+ORDER BY total_patients DESC;
+
+-- What is the total bill paid by each insurance provider?
+SELECT
+    p.insurance_provider,
+    SUM(b.insurance_bill) AS total_bill
+FROM gold.dim_patients p
+LEFT JOIN gold.fact_bills b
+    ON p.patient_id = b.Patient_id
+GROUP BY p.insurance_provider
+ORDER BY total_bill DESC;
+
+-- What is the total bill by each patient?
+SELECT
+    p.patient_id,
+    SUM(b.total_bill) AS total_bill
+FROM gold.dim_patients p
+LEFT JOIN gold.fact_bills b
+    ON p.patient_id = b.Patient_id
+GROUP BY p.patient_id
+ORDER BY total_bill DESC;
+
+-- What is the total amount by each payment_status?
+SELECT
+  payment_status,
+  SUM(total_bill) AS total_bill,
+	SUM(patient_bill) AS patient_bill,
+	SUM(insurance_bill) AS insurance_bill
+FROM gold.fact_bills
+GROUP BY payment_status
+ORDER BY total_bill DESC;
+
+-- What is the average amount by each payment_status?
+SELECT
+  payment_status,
+  AVG(total_bill) AS average_total_bill,
+	AVG(patient_bill) AS average_patient_bill,
+	AVG(insurance_bill) AS average_insurance_bill
+FROM gold.fact_bills
+GROUP BY payment_status
+ORDER BY total_bill DESC;
+
+-- Find total customers by countries
+SELECT
+    prestation,
+    SUM(quantity) AS total_by_prestation
+FROM gold.fact_medicaments
+GROUP BY prestation
+ORDER BY total_by_prestation DESC;
