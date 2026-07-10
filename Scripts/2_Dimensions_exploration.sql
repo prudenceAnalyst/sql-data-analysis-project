fd1d104a8cea6874f5bc08724674011935bc6ba7
prudenceAnalyst <prudencesinzo@gmail.com>
2026-07-10 03:20:08 +0200

    Update 2_Dimensions_exploration.sql

--- Scripts/2_Dimensions_exploration.sql
+++ Scripts/2_Dimensions_exploration.sql
@@ -11,17 +11,17 @@ SQL Functions Used:
 ===============================================================================
 */
 
--- Retrieve a list of unique patient_adrress from which patients originate
+-- Retrieve a list of unique patient's adress from where patients originate
 SELECT DISTINCT 
-    Patient_adrress
+    Adress
 FROM gold.dim_patients
-ORDER BY Patient_adrress;
+ORDER BY Adress;
 
--- Retrieve a list of unique patient's insurance provider
+-- Retrieve a list of unique patient's age range
 SELECT DISTINCT 
-    insurance_provider
+    age_range
 FROM gold.dim_patients
-ORDER BY insurance_provider;
+ORDER BY age_range;
 
 -- Retrieve a list of unique patient's Diagnosis
 SELECT DISTINCT 
@@ -29,14 +29,28 @@ SELECT DISTINCT
 FROM gold.dim_patients
 ORDER BY Diagnosis;
 
--- Retrieve a list of unique countries from which customers originate
+-- Retrieve a list of unique patient's insurance provider
+SELECT DISTINCT 
+    insurance_provider
+FROM gold.fact_bills
+ORDER BY insurance_provider;
+
+-- Retrieve a list of unique prestation
 SELECT DISTINCT 
     prestation
 FROM gold.fact_medicaments
 ORDER BY prestation;
 
--- Retrieve a list of unique department
+SELECT * FROM gold.fact_consultation
+
+-- Retrieve a list of unique prestation
 SELECT DISTINCT 
-    department 
-FROM gold.fact_medicaments
-ORDER BY department;
+    prestation
+FROM gold.fact_consultation
+ORDER BY prestation;
+
+-- Retrieve a list of unique prestation
+SELECT DISTINCT 
+    prestation
+FROM gold.fact_divers
+ORDER BY prestation;
